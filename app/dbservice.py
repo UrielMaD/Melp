56b11234aa2590c65c2129fa123bf58d8853f78f
UrielMaD <urielmd00@gmail.com>
2021-02-04 00:04:58 -0600

    First version of the melp API

---
+++ app/dbservice.py
@@ -0,0 +1,128 @@
+import mariadb
+import json
+
+try:
+    conn = mariadb.connect(
+        user="uriel",
+        password="",
+        host="localhost",
+        port=3306,
+        database="melp"
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+
+cur = conn.cursor()
+
+
+def _check_if_table_exists(name):
+    cur.execute(f"""
+        SHOW TABLES LIKE '{name}'
+        """)
+    result = cur.fetchone()
+    if result:
+        return True
+    else:
+        return False
+
+
+def _create_table():
+    cur.execute("""CREATE TABLE Restaurants( 
+        id VARCHAR(36) NOT NULL PRIMARY KEY,
+        rating INT,
+        name TEXT,
+        site TEXT,
+        email TEXT,
+        phone TEXT,
+        street TEXT,
+        city TEXT,
+        state TEXT,
+        lat FLOAT,
+        lng FLOAT
+    )
+    """)
+
+
+def get_restaurants():
+    table_exists = _check_if_table_exists('Restaurants')
+    if table_exists:
+        cur.execute("""SELECT * FROM Restaurants""")
+        headers = [x[0] for x in cur.description]
+        rv = cur.fetchall()
+        restaurants = []
+        for result in rv:
+            restaurants.append(dict(zip(headers, result)))
+        if len(restaurants) > 0:
+            return json.dumps(restaurants)
+        else:
+            return None
+    else:
+        _create_table()
+        return None
+
+
+def get_restaurant_by_id(id):
+    table_exists = _check_if_table_exists('Restaurants')
+    if table_exists:
+        cur.execute(f"SELECT * FROM Restaurants WHERE id = '{id}'")
+        headers = [x[0] for x in cur.description]
+        rv = cur.fetchall()
+        json_data = []
+
+        for result in rv:
+            json_data.append(dict(zip(headers, result)))
+        if len(json_data) > 0:
+            return json.dumps(json_data)
+        else:
+            return None
+    else:
+        return None
+
+
+def post_restaurant(rest):
+    if _check_if_table_exists('Restaurants') and get_restaurant_by_id(rest['id']) is None:
+        query = """INSERT INTO Restaurants VALUES (%s, %d, %s, %s, %s, %s, %s, %s, %s, %d, %d)"""
+        cur.execute(query, (
+            rest['id'], rest['rating'], rest['name'], rest['site'], rest['email'],
+            rest['phone'], rest['street'], rest['city'], rest['state'], rest['lat'],
+            rest['lng']
+        ))
+        conn.commit()
+        result = get_restaurant_by_id(rest['id'])
+        if result is not None:
+            return result
+        else:
+            return None
+    else:
+        return None
+
+
+def update_restaurant(id, rest):
+    if _check_if_table_exists('Restaurants') and get_restaurant_by_id(id) is not None:
+        query = """UPDATE Restaurants SET rating = %d, name = %s, site = %s, email = %s,
+                phone = %s, street = %s, city = %s, state = %s, lat = %d, lng = %d WHERE
+                id = %s"""
+        cur.execute(query, (
+            rest['rating'], rest['name'], rest['site'], rest['email'], rest['phone'],
+            rest['street'], rest['city'], rest['state'], rest['lat'], rest['lng'],
+            id,
+        ))
+        conn.commit()
+        result = get_restaurant_by_id(id)
+        if result is not None:
+            return result
+        else:
+            return None
+    else:
+        return None
+
+
+def delete_restaurant_by_id(id):
+    if get_restaurant_by_id(id):
+        cur.execute(f"DELETE FROM Restaurants WHERE id = '{id}'")
+        conn.commit()
+        return True
+    else:
+        return None
+
+
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import request, Response
+
+from app import create_app
+from app.dbservice import get_restaurants, get_restaurant_by_id, post_restaurant, \
+    delete_restaurant_by_id, update_restaurant
+
+app = create_app()
+
+
+@app.route('/restaurants', methods=['GET'])
+def get_all_restaurants():
+    return get_restaurants()
+
+
+@app.route('/restaurants/<id>', methods=['GET'])
+def restaurant_by_id(id):
+    return get_restaurant_by_id(id)
+
+
+@app.route('/restaurants', methods=['POST'])
+def add_restaurant():
+    request_data = request.get_json()
+    return post_restaurant(request_data)
+
+
+@app.route('/restaurants/<id>', methods=['DELETE'])
+def remove_restaurant(id):
+    if delete_restaurant_by_id(id):
+        response = Response("Restaurant deleted", status=200, mimetype='application/json')
+        return response
+    else:
+        response = Response("Restaurant not deleted", status=200, mimetype='application/json')
+        return response
+
+
+@app.route('/restaurants/<id>', methods=['PUT'])
+def update_restaurant_by_id(id):
+    request_data = request.get_json()
+    return update_restaurant(id, request_data)
+
+
+@app.route('/restaurants/statistics', methods=['GET'])
+def get_restaurants_by_radius():
+    latitude = request.arg.get('latitude')
+    longitude = request.arg.get('longitude')
+    radius = request.arg.get('radius')
